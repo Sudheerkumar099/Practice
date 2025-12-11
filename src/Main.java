ce93ad1fa5ffbef5ab64777a428f47beda00e8aa
Sudheerkumar099 <tsudheerkumar09@gmail.com>
2025-12-11 11:01:05 +0530

    .

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import java.io.PrintStream;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is added in Example Branch");        System.out.println("this is added in Example Branch");
-
+        System.out.println("this is added in Example Branch");
+        System.out.println("this is added in Example Branch");
 
     }
 }
